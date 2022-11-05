fd4fa267668b2aaaf4237606d9e3d20cf4cb3e9e
wastilla <wastilla@email.unc.edu>
2022-11-05 13:04:00 -0400

    Created Swift Files

---
+++ Roadio/RoadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadioApp.swift
+//  Roadio
+//
+//  Created by will astilla on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct RoadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
