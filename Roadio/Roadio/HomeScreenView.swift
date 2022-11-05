a1f528829c03d20dd49a2d470645962e415c9128
wastilla <wastilla@email.unc.edu>
2022-11-05 19:51:27 -0400

    Fixed Home Screen Map and began trip details view

--- Roadio/Roadio/HomeScreenView.swift
+++ Roadio/Roadio/HomeScreenView.swift
@@ -7,60 +7,38 @@
 
 import Foundation
 
+import CoreLocationUI
+import MapKit
 import SwiftUI
 
 struct HomeScreenView: View {
-    @State var carModel: String = ""
-    @State var location: String = ""
-    @State var destination: String = ""
-
+    @StateObject var locationManager = LocationManager()
     var body: some View {
         NavigationStack {
-            VStack(alignment: .center) {
-                VStack(spacing: 20) {
-                    HStack {
-                        Image(systemName: "circle.fill")
-                            .foregroundColor(.blue)
-                        TextField("Starting Point", text: $location)
-                            .font(.system(.title))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+            ZStack {
+                
+                Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
+                   
+                    .onAppear{
+                        locationManager.checkIfLocationServicesIsEnabled()
                     }
-                    HStack {
-                        Image(systemName: "mappin.and.ellipse")
-                            .foregroundColor(.red)
-                        TextField("Destination", text: $destination)
-                            .font(.system(.title))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                VStack {
+                    Spacer()
+                    
+                    NavigationLink(destination: TripDetailsView()) {
+                        Text("Start My Trip")
+                            .bold()
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 60)
+                            .background(Color("SickGreen"))
+                            .cornerRadius(10)
+                            .font(.system(.title, design: .rounded))
                     }
-                    HStack {
-                        Image(systemName: "car.fill")
-
-                        TextField("Car Model (i.e. camry)", text: $carModel)
-                            .font(.system(.title))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                    }
+                    .padding(.bottom, 50)
                 }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        print("Refresh")
-                    }) {
-                        Label("Send", systemImage: "paperplane.fill")
-                    }
-                }
-                ToolbarItem(placement: .principal) {
-                    Text("Logo")
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        print("Refresh")
-                    }) {
-                        Label("Refresh", systemImage: "arrow.clockwise")
-                    }
-                }
-            }
-            .padding()
+            .ignoresSafeArea()
+           
         }
     }
 }
